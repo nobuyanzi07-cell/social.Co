752e3ce3d6b3660884d230ece61a23a9115a9070
Nathan Oketch <nobuyanzi07@gmail.com>
2026-07-05 19:17:01 +0300

    Return the combinedObject

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function combineUser(...args) {
     const combinedObject = {
         user: {}
     };
-};
 
 for (const usernameArray of args) {
     combineObject.users = [...combinedObject.users, ...usernameArray];
 }
 
 combineObject.merge_date = Date.today().toString(`M/d/yyyy`);
+
+return combineObject;
+}
